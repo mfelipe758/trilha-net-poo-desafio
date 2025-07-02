7347d83db0da44c9426961241a8bb74ff111191a
FelipeM <almeida.felipejs@gmail.com>
2025-07-02 02:00:42 -0300

    desafio finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone nokia = new Nokia("11944155", "Modelo ultra", "dfd4656s64f", 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
+
+Console.WriteLine("------------------------");
+
+Smartphone iphone = new Iphone("83945454", "Modelo 16", "fd4h444d4hfh", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Facebook");
